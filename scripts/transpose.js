32e5efd692dd33de211d98847b1d3259f5f2537f
Luke Davis <99206365+lukewdavis@users.noreply.github.com>
2024-10-11 15:37:42 -0400

    Create transpose.js

---
+++ scripts/transpose.js
@@ -0,0 +1,7 @@
+function changeKey() {
+    const keySelect = document.getElementById('key-select');
+    const selectedKey = keySelect.value;
+
+    // Print the selected key to the console
+    console.log(`Selected Key: ${selectedKey}`);
+}
