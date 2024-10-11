e31370ece48563376068bf5f3ad04ce49d63e699
Luke Davis <99206365+lukewdavis@users.noreply.github.com>
2024-10-11 15:20:34 -0400

    Update transpose.js

--- scripts/transpose.js
+++ scripts/transpose.js
@@ -14,10 +14,6 @@ const chordMap = {
 };
 
 let originalLyrics = ''; // Declare this variable to hold the original lyrics
-let originalChords = [];
-
-console.log(originalLyrics);
-console.log(originalChords);
 
 function changeKey() {
     const keySelect = document.getElementById('key-select');
