e179a907ba1b82599455994bffd453d080a3ed61
Luke Davis <99206365+lukewdavis@users.noreply.github.com>
2024-10-14 11:07:04 -0400

    css and js changes/reorganization

---
+++ scripts/sort.js
@@ -0,0 +1,26 @@
+function sortList() {
+    var ul, li, switching, i, shouldSwitch, txtValueA, txtValueB;
+    ul = document.getElementById("songList");
+    li = ul.getElementsByTagName('li');
+    switching = true;
+
+    while (switching) {
+        switching = false;
+        for (i = 0; i < li.length - 1; i++) {
+            var a = li[i].getElementsByTagName("a")[0];
+            var b = li[i + 1].getElementsByTagName("a")[0];
+            txtValueA = a.textContent || a.innerText;
+            txtValueB = b.textContent || b.innerText;
+
+            if (txtValueA.toLowerCase() > txtValueB.toLowerCase()) {
+                shouldSwitch = true;
+                break;
+            }
+        }
+
+        if (shouldSwitch) {
+            li[i].parentNode.insertBefore(li[i + 1], li[i]);
+            switching = true;
+        }
+    }
+}
